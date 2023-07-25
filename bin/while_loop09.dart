117db02575e5e4e777e56155705b7011cd1ae01e
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-07-25 14:57:17 +0500

    while-9

--- bin/while_loop09.dart
+++ bin/while_loop09.dart
@@ -4,4 +4,18 @@
   
   
 */
-void main() {}
+
+func(int a){
+  String s = '';
+  int i = 0;
+  while (i<=a) {
+    if (i%2==0) {
+      s+=i.toString()+'';
+    }
+    i++;
+  }
+  return s;
+}
+void main() {
+  print(func(10));
+}
