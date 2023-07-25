9332999d013900c937bbb37e5fb6d560720142a8
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-07-25 14:57:32 +0500

    while-10

--- bin/while_loop10.dart
+++ bin/while_loop10.dart
@@ -6,4 +6,39 @@
   
   
 */
-void main() {}
+
+// func(int a){
+//   int i = 1;
+//   String x = '';
+//   while (i<=a){
+//     if (a%i==0) {
+//       x+=i.toString()+'tub son';
+//     } else {
+//       x+=i.toString() + 'tub son emas';
+//     }
+//     i++;
+//   }
+//   return x;
+// }
+
+
+
+
+String func(int a) {
+  if (a < 2) {
+    return "tub son emas";
+  }
+  
+  int i = 2;
+  while (i * i <= a) {
+    if (a % i == 0) {
+      return "tub son emas";
+    }
+    i++;
+  }
+  
+  return "tub son";
+}
+void main() {
+  print(func(7));
+}
