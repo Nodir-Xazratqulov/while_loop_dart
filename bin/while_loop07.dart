0054b6942eca7d990d408dbd320b7327f5975740
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-07-25 14:56:47 +0500

    while-7

--- bin/while_loop07.dart
+++ bin/while_loop07.dart
@@ -3,4 +3,33 @@
   Return odd and even numbers up to "a", whichever is smaller
   
 */
-void main() {}
+func(int a){
+  int count1 = 0;
+  int i = 0;
+  while (i<=a) {
+    if (i%2==1) {
+      count1+=1;
+    }
+    i++;
+  }
+
+  int count2 = 0;
+  int j = 0;
+  while (j<=a) {
+    if (j%2==1) {
+      count2+=1;
+    }
+    j++;
+  }
+
+  // if (count1>count2) {
+  //   return count2;
+  // } else {
+  //   return count1;
+  // }
+  return count1+count2;
+
+}
+void main() {
+  print(func(10));
+}
